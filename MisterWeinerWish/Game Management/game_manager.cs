61759b6faa0a88fe87256a67a6ff983be8217cd3
Mikha <42940272+alwod@users.noreply.github.com>
2024-01-24 17:30:45 +0000

    Added comments to game_manager.cs

--- MisterWeinerWish/Game Management/game_manager.cs
+++ MisterWeinerWish/Game Management/game_manager.cs
@@ -2,13 +2,16 @@ using Godot;
 using System;
 using System.Dynamic;
 
+// This script has been made an autoloaded script, and can be accessed in other scripts using 'GameManager'.
 public partial class game_manager : Node
 {
+	// This signal should be used to let other scripts know if the game is paused or not, so they don't need to check constantly in _Process.
 	[Signal]
 	public delegate void ToggleGamePausedEventHandler(bool isPaused);
 
 	private bool _isPaused = false;
 
+	// Getter and Setter for _isPaused. Setting GamePaused to anything will automatically emit the signal.
 	[Export]
 	bool GamePaused
 	{
