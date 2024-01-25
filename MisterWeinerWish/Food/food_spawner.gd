d232c61d489c3e7c7a12c2827fcafee06ab3bf82
Mikha <mikharohrs@gmail.com>
2024-01-25 21:50:21 +0000

    Food now randomly spawns on spawn points

--- MisterWeinerWish/Food/food_spawner.gd
+++ MisterWeinerWish/Food/food_spawner.gd
@@ -1,14 +1,21 @@
 extends Node
 
+const pickup = preload("res://Food/food_pickup.tscn")
+
+@export var foods: Array[FoodData]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
+	start_spawning_food()
 
 
 func start_spawning_food() -> void:
 	var spawn_points = get_tree().get_nodes_in_group("food_spawn_point")
-	
-	for spawn_point in spawn_points:
-		pass
-		
-		
+	var random = RandomNumberGenerator.new()
+	for spawn_point: Marker3D in spawn_points:
+		var food = foods[random.randi_range(0, foods.size() - 1)]
+		var food_pickup = pickup.instantiate()
+		food_pickup.food_data = food
+		food_pickup.position = spawn_point.position
+		add_child(food_pickup)
