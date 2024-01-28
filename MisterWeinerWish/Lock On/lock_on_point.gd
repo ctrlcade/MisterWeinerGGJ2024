a7bb63128f0413bf97d06de7609ff1d33961739c
Mikha <mikharohrs@gmail.com>
2024-01-28 13:34:40 +0000

    Added some lock-on point stuff

--- MisterWeinerWish/Lock On/lock_on_point.gd
+++ MisterWeinerWish/Lock On/lock_on_point.gd
@@ -1,8 +1,16 @@
 extends Node3D
+class_name LockOnPoint
 
 signal point_reached()
 
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
+func _process(delta: float) -> void:
+	pass
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	emit_signal("point_reached")
+
+
+func toggle_glow(isOn: bool) -> void:
+	mesh_instance_3d.show()
